773bc160ed677da0cf3e9a8c9d8bdefce52b93fc
DELL <s.hossam@nu.edu.eg>
2022-05-18 11:15:27 +0200

    Recent updates

---
+++ womenco_vendors/lib/view/homeScreens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold();
+      },
+    );
+  }
+}
